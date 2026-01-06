d8c31d19be78bfaae349bbcb0187b0c32e6350bd
Dilaxno <mail.esstafasoufiane@gmail.com>
2026-01-06 01:43:58 +0100

    calendly and templates

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, ForeignKey, JSON
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, ForeignKey, JSON, Text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from .database import Base
